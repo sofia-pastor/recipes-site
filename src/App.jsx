cb60a865f6c3322dce73fa7925a3e1aa151594bb
sofia-pastor <sofiapastor93@gmail.com>
2025-12-06 20:18:58 +0000

    added variables to Recipe page
    - maped json file and defined variables
    - organized the recipes by tag through reduce and sort

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import RecipePage from "./components/RecipePage";
+
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <p>Hello from App</p>
       <RecipePage />
     </div>
   );
--- src/components/RecipePage.jsx
+++ src/components/RecipePage.jsx
@@ -1,6 +1,44 @@
 import recipes from "../data/recipes.json";
 
 export default function RecipePage() {
-  const recipe = recipes;
-  console.log(recipe);
+  const recipeInfo = recipes.map((recipe, i) => ({
+    id: i + 1,
+    name: recipe.name,
+    img: recipe.img,
+    tags: recipe.tags,
+    link: recipe.link,
+    rate: recipe.rate,
+    time: recipe.time,
+    devices: recipe.devices,
+  }));
+
+  console.log(recipeInfo.flatMap((recipes) => recipes.tags));
+
+  //Agrupar por tag
+  const recipesByTag = recipeInfo.reduce((acc, recipe) => {
+    recipe.tags.forEach((tag) => {
+      if (!acc[tag]) {
+        acc[tag] = [];
+      }
+      acc[tag].push(recipe);
+    });
+    return acc;
+  }, {});
+
+  const tagNames = Object.keys(recipesByTag).sort((a, b) =>
+    a.localeCompare(b, "pt")
+  );
+
+  console.log(recipesByTag);
+  return (
+    <div className="RecipePage">
+      <h1>Receitas</h1>
+      <h2>Cookidoo</h2>
+      <ul>
+        {recipeInfo.map((recipe) => (
+          <li key={recipe.id}>{recipe.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
